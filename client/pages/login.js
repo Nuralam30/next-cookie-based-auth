a5e9929012f0908524c568f5446ac661bb100daf
Nuralam30 <nuralam.hstu.930@gmail.com>
2023-03-20 12:43:45 +0600

    login and registration page created

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,8 +1,37 @@
+import React, { useState } from 'react';
 
 const Login = () => {
+
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+
     return (
         <div>
-            <h1 className='jumbotron text-center bg-primary'>this is login page</h1>
+            <h1 className='jumbotron text-center bg-primary'>Login page</h1>
+
+            <div className="container p-2 pt-4 pb-4 col-md-4 offset-md-4">
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        className='form-control mb-4' 
+                        type="email" 
+                        placeholder='abc@gmail.com' 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required 
+                    />
+
+                    <input 
+                        className='form-control mb-4' 
+                        type="password" 
+                        placeholder='Enter password' 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required 
+                    />
+
+                    <button className='btn w-100 btn-primary'>Login</button>
+                </form>
+            </div>
         </div>
     );
 };
--- client/pages/register.js
+++ client/pages/register.js
@@ -1,8 +1,63 @@
+import React, { useState } from 'react';
 
 const Register = () => {
+
+    const [ name, setName ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ cPassword, setCpassword ] = useState("");
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+        console.table(name, email, password)
+    }
+
     return (
         <div>
-            <h1 className='jumbotron text-center bg-primary'>this is Register page</h1>
+            <h1 className='jumbotron text-center bg-primary'>Register page</h1>
+
+            <div className="container p-2 pt-4 pb-4 col-md-4 offset-md-4">
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        className='form-control mb-4' 
+                        type="text" 
+                        placeholder='Enter Your Name' 
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required 
+                    />
+
+                    <input 
+                        className='form-control mb-4' 
+                        type="email" 
+                        placeholder='abc@gmail.com' 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required 
+                    />
+
+                    <input 
+                        className='form-control mb-4' 
+                        type="password" 
+                        placeholder='Enter password' 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required 
+                    />
+
+                    <input 
+                        className='form-control mb-4' 
+                        type="password" 
+                        placeholder='Confirm password' 
+                        value={cPassword}
+                        onChange={(e) => setCpassword(e.target.value)}
+                        required 
+                    />
+
+                    <button className='btn w-100 btn-primary'>Register</button>
+                </form>
+            </div>
         </div>
     );
 };
