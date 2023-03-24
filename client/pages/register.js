73b5456431fbc199ec1ae25bde59311bf7b66a62
Nuralam30 <nuralam.hstu.930@gmail.com>
2023-03-24 12:09:08 +0600

    local env file added for next

--- client/pages/register.js
+++ client/pages/register.js
@@ -23,7 +23,7 @@ const Register = () => {
                     email,
                     password
                 }
-                const response = await axios.post('http://127.0.0.1:8000/api/register', newUser);
+                const response = await axios.post(`${process.env.NEXT_PUBLIC_API}/register`, newUser);
             }else{
                 window.alert('Password doesnot match');
             }
